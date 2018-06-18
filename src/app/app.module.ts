607c64d958bd49f149f44da73013113097e6953a
Filippov <filippov@teleformis.ru>
2018-06-18 17:53:48 +0300

    node-saas err

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MainPanelComponent } from './components/main-panel/main-panel.component';
+import { SvgLayoutComponent } from './components/svg-layout/svg-layout.component';
+import { LeftToolbarComponent } from './components/left-toolbar/left-toolbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPanelComponent,
+    SvgLayoutComponent,
+    LeftToolbarComponent
   ],
   imports: [
     BrowserModule
