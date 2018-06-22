5c30aebb2f00919e496a95a1ddf7e175c26166da
Filippov <filippov@teleformis.ru>
2018-06-22 22:08:01 +0300

    add ng class for draw tool selector, add basic logic, slack mode activated

--- src/app/components/left-toolbar/left-toolbar.component.ts
+++ src/app/components/left-toolbar/left-toolbar.component.ts
@@ -1,4 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+const drawTools: any = {
+  1: {
+    name: 'pencil',
+    id: 1,
+    pixels: 1
+  },
+  2: {
+    name: 'brush',
+    id: 2,
+    pixels: 1
+  },
+  3: {
+    name: 'arm',
+    id: 3,
+    pixels: null
+  },
+  4: {
+    name: 'eraser',
+    id: 4,
+    pixels: 1
+  }
+};
 
 @Component({
   selector: 'ngp-left-toolbar',
@@ -6,6 +29,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['left-toolbar.component.scss']
 })
 export class LeftToolbarComponent implements OnInit {
+  selectedDrawToolId: number = 1;
 
   constructor() { }
 
@@ -13,7 +37,10 @@ export class LeftToolbarComponent implements OnInit {
   }
 
   selectDrawTool(e) {
-    console.log(e);
+    let drawToolId: number = parseInt(e.currentTarget.getAttribute('data-drawtoolid'));
+    if (this.selectedDrawToolId !== drawToolId) {
+      this.selectedDrawToolId = drawToolId;
+    }
   }
 
 }
